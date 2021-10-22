ee7af7dcb65b400cd68c63de1d94c14034df376e
Isaiah St. John <hello@isaiah.st>
2021-10-22 11:17:38 -0400

    autocommit

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ bridge_box = st.empty()
 for s, step in range(bridge):
     bridge[s] = random.choice([0, 10])
     bridge_box.bar_chart(bridge)
-    if bridge[s] = 0:
+    if bridge[s] == 0:
         break
